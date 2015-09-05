5022054d66f184d8e1771e06438c87f567b8f81e
Gina Marie Holden <gholden3@deepthought19.cc.gatech.edu>
2015-09-05 19:18:18 -0400

    send one row at a time to 1DTransform

--- fft2d.cc
+++ fft2d.cc
@@ -23,20 +23,28 @@ char buf[MSG_SIZE]; //message buffer to send and recieve data from other CPUs
 using namespace std;
 
 
+
+void Transform1D(Complex* h, int w, Complex* H)
+{
+  // Implement a simple 1-d DFT using the double summation equation
+  //   // given in the assignment handout.  h is the time-domain input
+  //     // data, w is the width (N), and H is the output array.
+}
+
 void Transform2D(const char* inputFN) 
 { 
 
   int w, h;
   int numtasks, rank;
 
+  Complex* myData;  
+  Complex* resultArray;
+  Complex* hPtr;
+  Complex* HPtr;
+
   ofstream ofs("outFileG.txt"); //output file for debugging and testing
   ofs << "Hello world!" << endl;
-  Complex* myData;
  // Do the 2D transform here.
-  // 3) Allocate an array of Complex object of sufficient size to
-  //    hold the 2d DFT results (size is width * height)
-  // 4) Obtain a pointer to the Complex 1d array of input data
-  // 5) Do the individual 1D transforms on the rows assigned to your CPU
   // 6) Send the resultant transformed values to the appropriate
   //    other processors for the next phase.
   // 6a) To send and receive columns, you might need a separate
@@ -52,23 +60,39 @@ void Transform2D(const char* inputFN)
   InputImage image(inputFN);  // Create the helper object for reading the image
   w = image.GetWidth();
   h = image.GetHeight();
-  myData = image.GetImageData();
 
   // 2) Use MPI to find how many CPUs in total, and which one this process is
   MPI_Comm_size(MPI_COMM_WORLD, &numtasks); //how many CPUs? 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank); //which CPU am I?
   ofs  << "Number of tasks= " << numtasks << ".  My rank= " << rank<< endl;
 
-  image.SaveImageData("outData.txt", myData, w, h);
-  //outFileG.close();
+  // 3) Allocate an array of Complex object of sufficient size to
+  //     hold the 2d DFT results (size is width * height)
+  resultArray = new Complex[w*h]; //output array. 
+
+  // 4) Obtain a pointer to the Complex 1d array of input data
+  myData = image.GetImageData();
+
+  // 5) Do the individual 1D transforms on the rows assigned to your CPU
+  //lets just do all on one CPU to start.
+  for (int r = 0; r < h; ++r)
+    { // for each row
+    hPtr = myData + r * w; //where to start reading that row's input data
+    HPtr = resultArray + r * w; //where to start writing that row's output data
+    //print out the row to make sure we are sending it correctly
+    ofs  << "sending row: " << r << "." << endl;
+    for (int c = 0; c < w; ++c)
+      { // for each column
+      ofs << hPtr[c].Mag() << " ";
+      }
+    ofs << endl;
+
+    Transform1D(hPtr, w, HPtr); //transform that row and write into resultArray
+    }
+
+  image.SaveImageData("outData.txt", resultArray, w, h);
 }
 
-void Transform1D(Complex* h, int w, Complex* H)
-{
-  // Implement a simple 1-d DFT using the double summation equation
-  // given in the assignment handout.  h is the time-domain input
-  // data, w is the width (N), and H is the output array.
-}
 
 int main(int argc, char** argv)
 { 
