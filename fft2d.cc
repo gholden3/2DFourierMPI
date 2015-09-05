d0aaafeafb1e91e50e0f73bdcf046419e6b0d733
Gina Marie Holden <gholden3@deepthought19.cc.gatech.edu>
2015-09-05 18:28:21 -0400

    start MPI processes

--- fft2d.cc
+++ fft2d.cc
@@ -10,23 +10,29 @@
 #include <vector>
 #include <signal.h>
 #include <math.h>
-//#include <mpi.h>
+#include <mpi.h>
+#include <stdlib.h>
 
 #include "Complex.h"
 #include "InputImage.h"
+
+#define MSG_SIZE 1000
+
+char buf[MSG_SIZE]; //message buffer to send and recieve data from other CPUs
 
 using namespace std;
 
 
 void Transform2D(const char* inputFN) 
 { 
+
   int w, h;
+  int numtasks, rank;
+
+  ofstream ofs("outFileG.txt"); //output file for debugging and testing
+  ofs << "Hello world!" << endl;
   Complex* myData;
  // Do the 2D transform here.
-  // 1) Use the InputImage object to read in the Tower.txt file and
-  //    find the width/height of the input image.
-  // 2) Use MPI to find how many CPUs in total, and which one
-  //    this process is
   // 3) Allocate an array of Complex object of sufficient size to
   //    hold the 2d DFT results (size is width * height)
   // 4) Obtain a pointer to the Complex 1d array of input data
@@ -41,14 +47,20 @@ void Transform2D(const char* inputFN)
   //   9a) If you are CPU 0, collect all values from other processors
   //       and print out with SaveImageData().
 
-  // Step (1) in the comments is the line above.  
+  // 1) Use the InputImage object to read in the Tower.txt file and
+  //    find the width/height of the input image.
   InputImage image(inputFN);  // Create the helper object for reading the image
   w = image.GetWidth();
   h = image.GetHeight();
   myData = image.GetImageData();
 
-  // Your code here, steps 2-9
+  // 2) Use MPI to find how many CPUs in total, and which one this process is
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks); //how many CPUs? 
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); //which CPU am I?
+  ofs  << "Number of tasks= " << numtasks << ".  My rank= " << rank<< endl;
+
   image.SaveImageData("outData.txt", myData, w, h);
+  //outFileG.close();
 }
 
 void Transform1D(Complex* h, int w, Complex* H)
@@ -59,12 +71,21 @@ void Transform1D(Complex* h, int w, Complex* H)
 }
 
 int main(int argc, char** argv)
-{
+{ 
+  int rc ; 
   string fn("Tower.txt"); // default file name
   if (argc > 1) fn = string(argv[1]);  // if name specified on cmd line
   // MPI initialization here
+  rc = MPI_Init(&argc, &argv);
+  if(rc!=MPI_SUCCESS){
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+
   Transform2D(fn.c_str()); // Perform the transform.
+ 
   // Finalize MPI here
+  MPI_Finalize(); 
+ 
 }  
   
 
